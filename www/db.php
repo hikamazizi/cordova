2122f32f750f807ceca85960eee391d3dc18823d
hikamazizi <48471015+hikamazizi@users.noreply.github.com>
2019-05-20 14:12:19 +0700

    Update db.php

--- www/db.php
+++ www/db.php
@@ -1,4 +1,4 @@
 <?php
  header("Access-Control-Allow-Origin: *");
- $con = mysqli_connect("localhost","id7899373_admin","12345","id7899373_webservice") or die ("could not connect database");
+ $con = mysqli_connect("localhost","id7899369_hikam","12345","id7899369_0152") or die ("could not connect database");
 ?>
